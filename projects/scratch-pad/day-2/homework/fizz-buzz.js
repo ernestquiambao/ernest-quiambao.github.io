186ed0b65a4fc9bd95b7df9fdc43dc45fde42a8e
ernestquiambao <ernest.quiambao@gmail.com>
2022-11-17 18:13:13 +0000

    day 2 update

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -10,10 +10,26 @@
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
 */
+
+/*
+I: None
+O: logs numbers 1-100, logs "Fizz" for multiples of 3, logs "Buzz" for multiples of 5, logs "FizzBuzz" for numbers which are multiples of both 3 and 5
+C: N/A
+E: N/A
+*/
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-
-
+    for (var i = 1; i < 101; i++){// loops from 1-100
+        if (i % 3 === 0 && i % 5 === 0){//  checks if iteration number is a multiple of 3 and 5
+            console.log("FizzBuzz");//  logs "FizzBuzz"
+        }   else if (i % 3 === 0){//  checks if iteration number is a multiple of 3
+            console.log("Fizz");//  logs "Fizz"
+        }   else if (i % 5 === 0){//  checks if iteration number is a multiple of 5
+            console.log("Buzz");//  logs "Buzz"
+        }   else{//  any other condition
+            console.log(i);//  logs iteration number
+        }
+    }
 
     // YOUR CODE GOES ABOVE HERE //
 }
--- projects/scratch-pad/day-2/loops.js
+++ projects/scratch-pad/day-2/loops.js
@@ -10,11 +10,20 @@
  * Given an input Array, loop forward over the Array and print its values 
  * using console.log().
  */
+
+/*
+I: takes array as parameter
+O: logs array values
+C: N/A
+E: N/A
+*/
 function printArrayValues(array) {
   // YOUR CODE BELOW HERE //
-  
-  
-  
+  //  loops over all values of array
+  for (var i = 0; i < array.length; i++){
+  //  logs array value
+    console.log(array[i]);
+  }
   
   // YOUR CODE ABOVE HERE //
 }
@@ -23,11 +32,18 @@ function printArrayValues(array) {
  * Given an input Array, loop backwards over the Array and print its values 
  * using console.log().
  */
+
+/*
+I: takes array
+O: logs array values
+C: N/A
+E: N/A
+*/
 function printArrayValuesInReverse(array) {
   // YOUR CODE BELOW HERE //
-  
-  
-  
+  for (var i = array.length -1; i >= 0; i--){//  loops over array backwards
+    console.log(array[i]);//  logs array value
+  }
   
   // YOUR CODE ABOVE HERE //
 }
@@ -35,11 +51,19 @@ function printArrayValuesInReverse(array) {
 /** 
  * Given an input Object, return an Array containing the Object keys.
  */
+/*
+I: function takes object
+O:  function returns an array
+C: N/A
+E: N/A
+*/
 function getObjectKeys(object) {
   // YOUR CODE BELOW HERE //
-  
-  
-  
+  let array = [];// initialize array
+  for (var key in object){//  loops over object
+    array.push(key);//  logs keys
+  }
+  return array;// returns array
   
   // YOUR CODE ABOVE HERE //
 }
@@ -48,9 +72,18 @@ function getObjectKeys(object) {
  * Given an input Object, loop over the Object and print its keys 
  * using console.log().
  */
+
+/*
+I: object
+O: logs object's keys
+C: N/A
+E: N/A
+*/
 function printObjectKeys(object) {
   // YOUR CODE BELOW HERE //
-  
+  for (var key in object){//  loops over object
+    console.log(key);//  logs object's key to console
+  }
   
   
   
@@ -60,10 +93,20 @@ function printObjectKeys(object) {
 /** 
  * Given an input Object, return an Array containing the Object's values.
  */
+
+/*
+I: object
+O:  array containings object's values
+C: N/A
+E: N/A
+*/
 function getObjectValues(object) {
   // YOUR CODE BELOW HERE //
-  
-  
+  let array = [];// Initialize array
+  for (var key in object){//  loops over object
+    array.push(object[key]);// push object's values into array
+  }
+  return array;//  returns array
   
   
   // YOUR CODE ABOVE HERE //
@@ -73,9 +116,18 @@ function getObjectValues(object) {
  * Given an input Object, loop over the Object and print its values 
  * using console.log().
  */
+
+/*
+I: Object
+O: logs object's values
+C: N/A
+E: N/A
+*/
 function printObjectValues(object) {
   // YOUR CODE BELOW HERE //
-  
+  for (var key in object){//  loops over object
+    console.log(object[key]);//  logs object's values
+  }
   
   
   
@@ -85,10 +137,20 @@ function printObjectValues(object) {
 /** 
  * Given an input Object, return the number of key/value pairs stored within that Object.
  */
+
+/*
+I: object
+O: Number value of key/value pairs stored in object
+C: N/A
+E: N/A
+*/
 function getObjectLength(object) {
   // YOUR CODE BELOW HERE //
-  
-  
+  let array = [];//  Initialize array
+  for (var key in object){//  loops over object
+    array.push(key);// push key into array
+  }
+  return array.length;//return array length
   
   
   // YOUR CODE ABOVE HERE //
@@ -98,9 +160,22 @@ function getObjectLength(object) {
  * Given an input Object, how might we loop over the Object IN REVERSE and 
  * print its values using console.log()?
  */
+
+/*
+I: object
+O: print values in reverse
+C:  N/A
+E:  N/A
+*/
 function printObjectValuesInReverse(object) {
   // YOUR CODE BELOW HERE //
-  
+  let values = [];//  Initializing values
+  for (var key in object){
+    values.push(object[key]);//push values into values array
+  }
+  for (var i = values.length - 1; i >= 0; i--){//  Loop backwards through array
+    console.log(values[i]);//  log array value
+  }
   
   
   
