a3322e38f8bfd0e3eeff3c7a10629d191ac94896
ernestquiambao <ernest.quiambao@gmail.com>
2022-12-15 22:00:48 +0000

    asdf

--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -44,9 +44,36 @@ function every(array, func) {
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
-
-function dominantDirection() {
-
+//  we needed a data structure to keep count of ltr and rtl characters
+function dominantDirection(string) {
+// create arrays to store ltr and rtl
+let ltr = [];
+let rtl = [];
+//  iterate through string
+for (let i = 0; i < string.length; i++){
+  //  initialize value to hold individual characters in string
+    let script  = characterScript(string.charCodeAt(i));
+    //  test if character is not null
+    if (script !== null){
+      //  test if direction is ltrr
+      if (script.direction === 'ltr'){
+        // if true, push script into ltr array
+        ltr.push(script);
+        // if false
+      }else if (script.direction === 'rtl'){
+        //  push script into rtl array
+        rtl.push(script);
+      }
+  }
+} //  test if ltr length is greater than rtl length
+if (ltr.length > rtl.length){
+  //  return 'ltr'
+  return 'ltr';
+  //  else
+}else{
+  //  return 'rtl'
+  return 'rtl';
+}
 }
 
 // /////////////////////////////////////////////////////////////////////////////
